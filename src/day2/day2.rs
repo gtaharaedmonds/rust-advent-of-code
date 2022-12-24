d7c26c875dc25fc2b012eef191e7b3e378b8db46
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2022-12-23 19:08:58 -0800

    day3

--- src/day2/day2.rs
+++ src/day2/day2.rs
@@ -1,57 +1,98 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
-#[derive(PartialEq, Eq, Hash, Clone)]
+#[derive(PartialEq, Eq, Hash)]
 enum Throw {
     Rock,
     Paper,
     Scissors,
 }
 
-fn op_throw(throw: &char) -> Throw {
-    HashMap::from([
-        ('A', Throw::Rock),
-        ('B', Throw::Paper),
-        ('C', Throw::Scissors),
-    ])
-    .get(throw)
+#[derive(PartialEq, Eq)]
+enum GameResult {
+    Win,
+    Draw,
+    Loss,
+}
+
+fn decode_op_throw(throw: &char) -> Throw {
+    match throw {
+        'A' => Some(Throw::Rock),
+        'B' => Some(Throw::Paper),
+        'C' => Some(Throw::Scissors),
+        _ => None,
+    }
     .unwrap()
-    .to_owned()
 }
 
-fn my_throw(throw: &char) -> Throw {
-    HashMap::from([
-        ('X', Throw::Rock),
-        ('Y', Throw::Paper),
-        ('Z', Throw::Scissors),
-    ])
-    .get(throw)
+fn decode_my_throw(throw: &char) -> Throw {
+    match throw {
+        'X' => Some(Throw::Rock),
+        'Y' => Some(Throw::Paper),
+        'Z' => Some(Throw::Scissors),
+        _ => None,
+    }
     .unwrap()
-    .to_owned()
 }
 
-fn throw_score(throw: &Throw) -> i32 {
-    HashMap::from([(Throw::Rock, 1), (Throw::Paper, 2), (Throw::Scissors, 3)])
-        .get(throw)
-        .unwrap()
-        .to_owned()
-}
-
-fn score_throws(me: &Throw, op: &Throw) -> i32 {
-    let score_map = HashSet::from([
+fn game_result(me: &Throw, op: &Throw) -> GameResult {
+    let results = HashSet::from([
         (&Throw::Rock, &Throw::Scissors),
         (&Throw::Paper, &Throw::Rock),
         (&Throw::Scissors, &Throw::Paper),
     ]);
 
-    if score_map.contains(&(me, op)) {
-        // Win
-        6
-    } else if score_map.contains(&(op, me)) {
-        // Loss
-        0
+    if results.contains(&(me, op)) {
+        GameResult::Win
+    } else if results.contains(&(op, me)) {
+        GameResult::Loss
     } else {
-        // Draw
-        3
+        GameResult::Draw
+    }
+}
+
+fn score_result(result: &GameResult) -> i32 {
+    match result {
+        GameResult::Win => 6,
+        GameResult::Loss => 0,
+        GameResult::Draw => 3,
+    }
+}
+
+fn throw_score(throw: &Throw) -> i32 {
+    match throw {
+        Throw::Rock => 1,
+        Throw::Paper => 2,
+        Throw::Scissors => 3,
+    }
+}
+
+fn decode_game_result(result: &char) -> GameResult {
+    match result {
+        'X' => Some(GameResult::Loss),
+        'Y' => Some(GameResult::Draw),
+        'Z' => Some(GameResult::Win),
+        _ => None,
+    }
+    .unwrap()
+}
+
+fn throw_response(op: &Throw, result: &GameResult) -> Throw {
+    match op {
+        Throw::Rock => match result {
+            GameResult::Win => Throw::Paper,
+            GameResult::Loss => Throw::Scissors,
+            GameResult::Draw => Throw::Rock,
+        },
+        Throw::Paper => match result {
+            GameResult::Win => Throw::Scissors,
+            GameResult::Loss => Throw::Rock,
+            GameResult::Draw => Throw::Paper,
+        },
+        Throw::Scissors => match result {
+            GameResult::Win => Throw::Rock,
+            GameResult::Loss => Throw::Paper,
+            GameResult::Draw => Throw::Scissors,
+        },
     }
 }
 
@@ -59,10 +100,25 @@ pub fn day2_p1(input: &str) -> i32 {
     let mut score = 0;
 
     for line in input.lines() {
-        let line_vec: Vec<char> = line.chars().collect();
-        let op = op_throw(&line_vec[0]);
-        let me = my_throw(&line_vec[2]);
-        score += score_throws(&me, &op) + throw_score(&me);
+        let chars = line.chars().collect::<Vec<char>>();
+        let op = decode_op_throw(&chars[0]);
+        let me = decode_my_throw(&chars[2]);
+        let result = game_result(&me, &op);
+        score += score_result(&result) + throw_score(&me);
+    }
+
+    score
+}
+
+pub fn day2_p2(input: &str) -> i32 {
+    let mut score = 0;
+
+    for line in input.lines() {
+        let chars = line.chars().collect::<Vec<char>>();
+        let result = decode_game_result(&chars[2]);
+        let op = decode_op_throw(&chars[0]);
+        let me = throw_response(&op, &result);
+        score += score_result(&result) + throw_score(&me);
     }
 
     score
--- src/day2/main.rs
+++ src/day2/main.rs
@@ -2,13 +2,19 @@ use std::fs;
 mod day2;
 
 fn main() {
-    let p1 = fs::read_to_string("input/day2/input.txt").unwrap();
-    println!("Day 2 part 1 solution: {}", day2::day2_p1(&p1));
+    let contents = fs::read_to_string("input/day2/input.txt").unwrap();
+    println!("Day 2 part 1 solution: {}", day2::day2_p1(&contents));
+    println!("Day 2 part 2 solution: {}", day2::day2_p2(&contents));
 }
 
 #[test]
 fn test_day2_p1() {
     let contents = fs::read_to_string("input/day2/test.txt").unwrap();
-    let result = day2::day2_p1(&contents);
-    assert_eq!(result, 15);
+    assert_eq!(day2::day2_p1(&contents), 15);
+}
+
+#[test]
+fn test_day2_p2() {
+    let contents = fs::read_to_string("input/day2/test.txt").unwrap();
+    assert_eq!(day2::day2_p2(&contents), 12);
 }
